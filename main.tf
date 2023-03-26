d83aee0f65fc58cd8361665f3a1e3194c9779686
elvis.ngeh <evis.bantar@gmail.com>
2023-03-26 17:30:40 -0400

    new2

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+#  profile = "class30"
+}
+
+resource "aws_vpc" "actions" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+      "Name" = "class30"
+    }
+  
+}
